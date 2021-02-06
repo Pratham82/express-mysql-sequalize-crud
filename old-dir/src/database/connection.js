6dd8997caf92de0f4fa2f358a0445e9a3fc35cbe
pratham82 <mali.prathamesh82@gmail.com>
2021-02-07 02:18:33 +0530

    Removed old files

--- old-dir/src/database/connection.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('socialnetwork', 'pratham', 'rootedpirate', {
-  host: 'localhost',
-  dialect: 'mysql',
-  operatorAlias: false,
-})
-
-module.exports = sequelize
-global.sequelize = sequelize
