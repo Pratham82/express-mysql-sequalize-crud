9fb56f7fd1b0aaee1681a4e0e924a5e06bd2d41f
pratham82 <mali.prathamesh82@gmail.com>
2021-02-10 16:31:37 +0530

    Updated routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const createNewTodo = require('../controllers/createNewTodo')
+const deleteTodoById = require('../controllers/deleteTodoById')
+const getAllTodos = require('../controllers/getAllTodos')
+const getTodoById = require('../controllers/getTodoById')
+const updateTodoById = require('../controllers/updateTodoById')
 const router = express.Router()
-const db = require('../models')
 
-router.get('/all', (req, res) => {
-  db.Todo.findAll().then((todos) => res.send(todos))
-})
+router.get('/all', getAllTodos)
+router.post('/new', createNewTodo)
+router.get('/find/:id', getTodoById)
+router.delete('/delete/:id', deleteTodoById)
+router.put('/update/:id', updateTodoById)
 
 module.exports = router
